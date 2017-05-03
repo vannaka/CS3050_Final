30f31cb0edb40fc119edebdd9672d8476241f73a
Luke Mammen <luk3r0ck5@gmail.com>
2017-05-03 17:13:28 -0500

    stuff

--- CS3050_Final/main.c
+++ CS3050_Final/main.c
@@ -2,18 +2,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <ctype.h>
+
 #define MAXLINELENGTH 80
+#define HASHSIZE 100
 
 typedef struct capitalNode {
-	char* cityName;
+	char cityName[50];
+	char stateName[50];
 	int visited;
 	struct capitalNode *parent;
-	char* stateName;
 	struct capitalListNode* neighborCities;
+	struct capitalNode *nextHash;
 }capitalNode;
 
 typedef struct capitalListNode {
-	capitalNode* city;
+	capitalNode city[50];
 	int dist;
 	struct capitalListNode* next;
 }capitalListNode;
@@ -21,26 +25,43 @@ typedef struct capitalListNode {
 
 //functions
 void inputCapitals(char*);
-
-int isDigit(char* );
 capitalNode* newCapital(char* token, char* token2);
 capitalListNode* newCapitalListNode(char* token, int);
+int isNumber(char* input);
+capitalNode* getCapital(char* cityName);
+void insertHash(capitalNode* capital);
+int calcHash(char* city);
+void printAdjCities(capitalNode *capital);
 
 //global variables
-capitalNode** cities;
+capitalNode* cities[HASHSIZE];
 
 
 int main(int argc,  char* argv[]) {
-
-	cities = malloc(sizeof(capitalNode*) * 50);
-
 	//inputCapitals(argv[1]);
 	inputCapitals("capitalsnum.txt");
 
-	scanf("%d");
+	capitalNode *hold = getCapital("Jefferson City");
+	printAdjCities(hold);
+
+	getc(stdin);
+
 	return 0;
 }
 
+// used to test hash table
+void printAdjCities(capitalNode *capital) {
+	capitalListNode *hold = capital->neighborCities;
+
+	while (hold != NULL) {
+		printf("\n%s, %d", hold->city, hold->dist);
+		hold = hold->next;
+	}
+
+	printf("\n");
+}
+
+// build graph
 void inputCapitals(char* inputFile)
 {
 	FILE* input;
@@ -56,18 +77,20 @@ void inputCapitals(char* inputFile)
 	capitalNode* capital = NULL;
 	int capitalNumber = 0;
 
-	//parse out input file
+	// parse out input file
 	while (fgets(inputLine, MAXLINELENGTH, input) != NULL)
 	{
 		token = strtok(inputLine, "-");
 		token2 = strtok(NULL, "\n");
 
-		//token2 is a city name
-		if (isDigit(token2) == 0) 
+		// token2 is a city name
+		if (isNumber(token2) == 0)
 		{
 			if (capital != NULL)
 			{
-				cities[capitalNumber] = capital;
+				// add capitol to graph once all its adjacent nodes are read in
+				insertHash(capital);
+				//cities[capitalNumber] = capital;
 				capital = NULL;
 				
 				capitalNumber++;
@@ -78,9 +101,11 @@ void inputCapitals(char* inputFile)
 		// token2 is a distance
 		else
 		{
-			//create capital list node
+			// create capital list node
 			int distance = atoi(token2);
 			capitalListNode* newNeighbor = newCapitalListNode(token, distance);
+
+			// add city to adjacent list
 			if (capital->neighborCities == NULL) 
 			{
 				capital->neighborCities = newNeighbor;
@@ -98,46 +123,86 @@ void inputCapitals(char* inputFile)
 	}
 }
 
-capitalNode* newCapital(char* token, char* token2)
+capitalNode* newCapital(char* state, char* city)
 {
-	//copy strings over to new strings to be used in the node
-	char* stateName = malloc(sizeof(char) * 50);
-	char* cityName = malloc(sizeof(char) * 50);
-	strcpy(stateName, token);
-	strcpy(cityName, token2);
-
 	capitalNode* newNode = malloc(sizeof(capitalNode));
-	newNode->stateName = stateName;
-	newNode->cityName = cityName;
+	strcpy(newNode->stateName, state);
+	strcpy(newNode->cityName, city);
 	newNode->neighborCities = NULL;
+	newNode->nextHash = NULL;
 
 	return newNode;
 }
-capitalListNode* newCapitalListNode(char* token, int distance)
+capitalListNode* newCapitalListNode(char* city, int distance)
 {
-	char* cityName = malloc(sizeof(char) * 50);
-	strcpy(cityName, token);
-
 	capitalListNode* newNode = malloc(sizeof(capitalListNode));
-	newNode->city = cityName;
+	strcpy(newNode->city, city);
 	newNode->dist = distance;
 	newNode->next = NULL;
 
 	return newNode;
 }
 
-int isDigit(char* input)
-{
-	int digit = 0;
-	unsigned int i = 0;
+// check if all the chars in a string are numbers
+// return 0 if any part of the string contains something other than a number, else return 1
+int isNumber(char* input) {
+	int len = strlen(input);
+	int i;
 
-	while (i < strlen(input))
-	{
-		digit = isdigit(input[i]);
-	
-		if (digit == 0)
-			return digit;
-		i++;
+	for (i = 0; i < len; i++) {
+		if (isdigit(input[i]) == 0) // the char is not a digit
+			return 0;
 	}
-	return digit;
+
+	return 1;
+}
+
+// calculate the hash index for a city
+int calcHash(char* city) {
+	int len = strlen(city);
+	int i, hold = 0;
+
+	for (i = 0; i < len; i++) {
+		hold += (int)city[i];
+	}
+
+	return hold % HASHSIZE;
+}
+
+// insert a city into the hash table
+void insertHash(capitalNode* capital) {
+	int hash = calcHash(capital->cityName);
+	capitalNode *hold;
+
+	// check if spot is empty
+	if (cities[hash] == NULL) {
+		cities[hash] = capital;
+	}
+	// if not add to end of linked list
+	else {
+		hold = cities[hash];
+		while (hold->nextHash != NULL) {
+			hold = hold->nextHash;
+		}
+		hold->nextHash = capital;
+	}
+}
+
+// search hash table for a city, return null if it doesnt exist
+capitalNode* getCapital(char* cityName) {
+	int hash = calcHash(cityName);
+	capitalNode *hold;
+
+	// check if spot is empty
+	if (cities[hash] == NULL) {
+		return NULL;	// the city is not in the table (graph)
+	}
+	// if not add to end of linked list
+	else {
+		hold = cities[hash];
+		while (hold->nextHash != NULL) {
+			hold = hold->nextHash;
+		}
+		return hold;	// we found the city we are looking for
+	}
 }
